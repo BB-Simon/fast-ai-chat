41266bfe1c6b98c1fd5d7b3111e2d93a1333df1a
BB-Simon <simon.chowdery@gmail.com>
2026-04-30 17:04:04 +0300

    Add email and password validation

--- app/schemas/auth_schema.py
+++ app/schemas/auth_schema.py
@@ -1,9 +1,27 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, field_validator
+import re
 
 class SignupRequest(BaseModel):
-  email: str
+  email: EmailStr
   password: str
 
+  @field_validator("password")
+  def validate_password(cls, val):
+    if len(val) < 8:
+      raise ValueError("Password must be at lest 8 charecters")
+    
+    if not re.search(r"[A-Z]", val):
+      raise ValueError("Password must inclue uppercase letter")
+    
+    if not re.search(r"[a-z]", val):
+      raise ValueError("Password must include lowarecase letter")
+    
+    if not re.search(r"0-9", val):
+      raise ValueError("Password must include a number")
+    
+    return val
+
+
 class LoginRequest(BaseModel):
-  email: str
+  email: EmailStr
   password: str
